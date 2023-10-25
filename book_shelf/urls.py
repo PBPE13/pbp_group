e2bcd3178f8b3173b7b626530fbe4f42e9032b52
RafGhan <rgharditama@gmail.com>
2023-10-25 15:38:24 +0700

    update urls.py book

--- book_shelf/urls.py
+++ book_shelf/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path("api/books/", include("book.urls"))
+    path("api/books/", include("book.urls")),
 ]
