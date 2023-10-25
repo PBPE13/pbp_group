127d768f41bf183bf2cf129085fac1e849127f30
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-10-25 13:02:48 +0700

    modified models

--- main/models.py
+++ main/models.py
@@ -8,6 +8,11 @@ class Book(models.Model):
     genre = models.CharField(max_length=100)
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    bio_data = models.TextField(blank=True) #optional
+    preferred_genre = models.CharField(max_length=100, blank=True) #optional
+
+    def __str__(self):
+        return self.user.username
