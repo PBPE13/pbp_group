3d9e8c9386cb0ae43bd99e886d7d43c6db9c339a
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-10-26 15:26:32 +0700

    added book.json file

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from book.views import get_books
 
 app_name = 'book'
 
 urlpatterns = [
-    #path("", get_books, name="get_books"),
+    path("", get_books, name="get_books"),
 ]
--- book/views.py
+++ book/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.core import serializers
 from book.models import Book
 
-# Create your views here.
-#def get_books(request):
-#    data = Book.objects.all
-#    return HttpResponse(serializers.serialize("json", data), content_type= "application/json" )
+def get_books(request):
+    data = Book.objects.all
+    return HttpResponse(serializers.serialize("json", data), content_type= "application/json" )
