2964db07e6cf87bce9f36303508ec4f27eda0b1c
RafGhan <rgharditama@gmail.com>
2023-10-29 12:08:33 +0700

    update borrow.html, views, url and make form and book_list.html

--- borrow_book/urls.py
+++ borrow_book/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import show_borrow
+from .views import show_borrow, get_borrow, borrow_book, return_book
 
 app_name = 'borrow_book'  
 
 urlpatterns = [
-    path('show-borrow', show_borrow, name='show_borrow'),
+    path('', show_borrow, name='show_borrow'),
+    path('get-borrow/', get_borrow, name='get_borrow'),
+    path('borrow-book', borrow_book, name = 'borrow_book'),
+    path('return-book', return_book, name='return_book')
 ]
