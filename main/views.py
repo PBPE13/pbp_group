56beee3192674bbece434e699a3e7614f597dd25
KenishaJazlyn <kenishajazlyn@gmail.com>
2023-12-06 16:04:54 +0700

    edit csrf trusted origin

--- main/views.py
+++ main/views.py
@@ -17,7 +17,7 @@ def show_main(request):
 
 def show_home(request):
     return render(request, "home.html")
-@csrf_protect
+@csrf_exempt
 def register(request):
     form = RegisterForm()
 
@@ -36,7 +36,7 @@ def register(request):
             return redirect('main:login')
     context = {'form':form}
     return render(request, 'register.html', context)
-@csrf_protect
+@csrf_exempt
 def login_user(request):
     form = LoginForm()
     if request.method == 'POST':
