aa190ea7a6e4d7e8124318c896b1eface29513f6
KenishaJazlyn <kenishajazlyn@gmail.com>
2023-12-09 07:26:33 +0700

    add register

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from authentication.views import login, logout
+from authentication.views import login, logout, flutter_register
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),path('logout/', logout, name='logout'),
+    path('registerFlutter/', flutter_register, name='register-flutter'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
-
+from django.contrib.auth.models import User
+from main.models import Profile
 
 @csrf_exempt
 def login(request):
@@ -35,7 +36,6 @@ def login(request):
 @csrf_exempt
 def logout(request):
     username = request.user.username
-
     try:
         auth_logout(request)
         return JsonResponse({
@@ -48,3 +48,34 @@ def logout(request):
         "status": False,
         "message": "Logout gagal."
         }, status=401)
+    
+@csrf_exempt
+def flutter_register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password']
+        password2 = request.POST['password2']
+        description = request.POST['description']
+        role = request.POST['role']
+        genre = request.POST['genre']
+        fullname = request.POST['fullname']
+      
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"status": "duplicate"}, status=401)
+
+        if password1 != password2:
+            return JsonResponse({"status": "pass failed"}, status=401)
+        
+        new_user = User.objects.create_user(username=username, password=password1)
+        print(username + " pass " + password1+"  ..... " + password2)
+        Profile.objects.create(
+            user = new_user,
+            role = role,
+            name = fullname,
+            bio_data = description,
+            preferred_genre =genre,   
+        )
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
